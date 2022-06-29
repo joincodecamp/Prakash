26cd8249bea0cd52392d45469664af93d1a21598
Prakash Chand <rajputpc163@hotmail.com>
2022-06-29 18:33:53 +0530

    Contact List Done

--- contactlist/script.js
+++ contactlist/script.js
@@ -1,7 +1,7 @@
 
 var contactDetails = [];
 let edit = false;
-let currentId;
+let currentId, currentIndex, globalProperty;
 
 function renderDetail() {
     const table = document.getElementById("detail-table");
@@ -10,7 +10,6 @@ function renderDetail() {
     for (let i = 0; i < contactDetails.length; i++) {
 
         const itemID = contactDetails[i].id;
-
         let row = table.insertRow();
         let cell1 = row.insertCell(0);
         let cell2 = row.insertCell(1);
@@ -25,15 +24,35 @@ function renderDetail() {
         cell5.innerHTML = `<i onclick="editRow('${itemID}')">Edit</i><i onclick="deleteRow('${itemID}')">Delete</i>`;
         //console.log(table);
 
+        cell1.addEventListener('click', function () {
+            addProperty(i, 'fname');
+            document.getElementById('contact-form').style.display = 'none';
+            document.getElementById('update-form').style.display = 'block';
+        });
+        cell2.addEventListener('click', function () {
+            addProperty(i, 'address');
+            document.getElementById('contact-form').style.display = 'none';
+            document.getElementById('update-form').style.display = 'block';
+        });
+        cell3.addEventListener('click', function () {
+            addProperty(i, 'number');
+            document.getElementById('contact-form').style.display = 'none';
+            document.getElementById('update-form').style.display = 'block';
+        });
+        cell4.addEventListener('click', function () {
+            addProperty(i, 'email');
+            document.getElementById('contact-form').style.display = 'none';
+            document.getElementById('update-form').style.display = 'block';
+        });
     }
+
     return false;
 }
 
+//submit form data
 function submitF() {
     const isValid = validation(); //validation
     if (isValid) {
-        //console.log("contactDetails", contactDetails);
-
         if (edit) {
             updateRow();
             //alert("List Update");
@@ -67,11 +86,11 @@ function submitF() {
             return false;
         }
     } else {
-        //console.log("contactDetails", contactDetails);
         return false;
     }
 }
-//edit row
+
+//edit row data
 function editRow(id) {
     contactDetails.map((item, i) => {
         if (item.id == id) {
@@ -83,51 +102,85 @@ function editRow(id) {
         }
     });
     edit = true;
-    // console.log(edit);
-    // console.log(contactDetails);
 }
 
-//update
+//reupdate values
+function addProperty(i, currentProperty) {
+    currentIndex = i;
+    globalProperty = currentProperty;
+    document.getElementById("update").value = contactDetails[i][currentProperty];
+}
+
+//update single data
+function updateData() {
+    contactDetails[currentIndex][globalProperty] = document.getElementById('update').value;
+    renderDetail();
+    document.getElementById("update").value = '';
+    return false;
+}
+
+//update data
 function updateRow() {
+
+    //normal edit update
     contactDetails[currentId].fname = document.getElementById('fname').value;
     contactDetails[currentId].address = document.getElementById('address').value;
     contactDetails[currentId].number = document.getElementById('number').value;
     contactDetails[currentId].email = document.getElementById('email').value;
-
+    edit = false;
+    renderDetail();
     document.getElementById("fname").value = '';
     document.getElementById("address").value = '';
     document.getElementById("number").value = '';
     document.getElementById("email").value = '';
-    edit = false;
-    // console.log(edit);
-    // console.log(contactDetails);
-    renderDetail();
 }
+
+
 
 //delete row
 function deleteRow(id) {
     var item = contactDetails.filter((item) => item.id != id);
     contactDetails = item;
     renderDetail();
-    //console.log(contactDetails);
+    document.getElementById("fname").value = '';
+    document.getElementById("address").value = '';
+    document.getElementById("number").value = '';
+    document.getElementById("email").value = '';
 }
 
 
 //form validation
 function validation() {
-
     const fname = document.getElementById('fname').value;
     const address = document.getElementById('address').value;
     const number = document.getElementById('number').value;
     const email = document.getElementById('email').value;
 
-    if (fname == '' || address == '' || number == '' || email == '') {
+    if (fname == "") {
         document.getElementById("fname-hide").classList.add("error");
+        return false;
+    }
+    if (address == "") {
         document.getElementById("add-hide").classList.add("error");
+        return false;
+    }
+    if (number == "") {
         document.getElementById("number-hide").classList.add("error");
+        return false;
+    }
+    if (email == "") {
         document.getElementById("mail-hide").classList.add("error");
         return false;
     }
+
+
+    // if (fname == '' || address == '' || number == '' || email == '') {
+    //     document.getElementById("fname-hide").classList.add("error");
+    //     document.getElementById("add-hide").classList.add("error");
+    //     document.getElementById("number-hide").classList.add("error");
+    //     document.getElementById("mail-hide").classList.add("error");
+    //     return false;
+    // }
 
     if (contactDetails.length > 0) {
         const numberValue = contactDetails.find((item) =>
@@ -145,52 +198,19 @@ function validation() {
         }
 
         const emailValue = contactDetails.find((item) =>
-        item.email == email
-    );
-    if (emailValue) {
-        alert("Email already exist...");
-        document.getElementById("fname").value = '';
-        document.getElementById("address").value = '';
-        document.getElementById("number").value = '';
-        document.getElementById("email").value = '';
-        return false;
-    } else {
-        // alert("No Matching email found...");
+            item.email == email
+        );
+        if (emailValue) {
+            alert("Email already exist...");
+            document.getElementById("fname").value = '';
+            document.getElementById("address").value = '';
+            document.getElementById("number").value = '';
+            document.getElementById("email").value = '';
+            return false;
+        } else {
+            // alert("No Matching email found...");
+        }
     }
-    }
-
-    // if (contactDetails.length > 0) {
-    //     contactDetails.find((item) => {
-    //         if (item.email == email) {
-    //             alert("Email Match");
-    //             return false;
-    //         }
-    //         else if (item.number == number) {
-    //             alert("Number Match");
-    //             return false;
-    //         }else{
-    //             alert("Not Match");
-    //         }
-    //     } );
-    // }
-
-    // if (fname == "") {
-    //     document.getElementById("fname-hide").classList.add("error");
-    //     return false;
-    // }
-    // if (address == "") {
-    //     document.getElementById("add-hide").classList.add("error");
-    //     return false;
-    // }
-    // if (number == "") {
-    //     document.getElementById("number-hide").classList.add("error");
-    //     return false;
-    // }
-    // if (email == "") {
-    //     document.getElementById("mail-hide").classList.add("error");
-    //     return false;
-    // }
-
     return true;
 }
 
@@ -207,3 +227,10 @@ function textEmptytrd() {
 function textEmptyfrth() {
     document.getElementById("mail-hide").classList.remove("error");
 }
+
+//display form
+let addDetail = document.getElementById("hide-click");
+addDetail.addEventListener('click', function () {
+    document.getElementById('contact-form').style.display = 'block';
+    document.getElementById('update-form').style.display = 'none';
+});
